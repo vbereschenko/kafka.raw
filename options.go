041ca3b4281870c7fe13fbeaabfbac65120cc423
ilya <iKrasheninnikov@coins.exchange>
2019-01-18 15:12:25 +0200

    update dependencies

---
+++ options.go
@@ -0,0 +1,35 @@
+package kafka_raw
+
+import (
+	"context"
+
+	"github.com/Shopify/sarama"
+	"github.com/micro/go-micro/broker"
+	sc "gopkg.in/bsm/sarama-cluster.v2"
+)
+
+var (
+	DefaultBrokerConfig  = sarama.NewConfig()
+	DefaultClusterConfig = sc.NewConfig()
+)
+
+type brokerConfigKey struct{}
+type clusterConfigKey struct{}
+
+func BrokerConfig(c *sarama.Config) broker.Option {
+	return func(o *broker.Options) {
+		if o.Context == nil {
+			o.Context = context.Background()
+		}
+		o.Context = context.WithValue(o.Context, brokerConfigKey{}, c)
+	}
+}
+
+func ClusterConfig(c *sc.Config) broker.Option {
+	return func(o *broker.Options) {
+		if o.Context == nil {
+			o.Context = context.Background()
+		}
+		o.Context = context.WithValue(o.Context, clusterConfigKey{}, c)
+	}
+}
